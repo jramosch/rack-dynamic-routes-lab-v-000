4d775b46474cee812d694527473b68b7955dd4b2
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-27 03:10:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/items/").last
     else
-      resp.write "Route Not Found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
