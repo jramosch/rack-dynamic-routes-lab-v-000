689704a6ae2c66d9c20831bdb22012a8f9036a23
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-27 03:21:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      name = req.path.split("/items/").last
-      if @@items.include?(name)
-        item = @@items.find{|i| i.name == name}
+      item_name = req.path.split("/items/").last
+      if @@items.include?(item_name)
+        item = @@items.find{|i| i.name == item_name}
         resp.write item.price
         resp.status = 200
       else
